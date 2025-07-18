45d2f8efa8c0d5a58d537e69dbce32259144a813
Matheus Reinaldo <160988681+reinaldo-matheus@users.noreply.github.com>
2025-07-18 18:31:03 +0000

    adiciona arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Ei maninho, isso não é bruxaria é apenas seu mano, desbloqueando novas fases do desenvolvimento web 😝🪄</h1>
+function Home() {
+  return (
+    <h1>
+      Ei maninho, isso não é bruxaria é apenas seu mano, desbloqueando novas
+      fases do desenvolvimento web 😝🪄
+    </h1>
+  );
 }
 
 export default Home;
