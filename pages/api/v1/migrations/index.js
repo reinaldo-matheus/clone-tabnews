f528c0d4a9a05ffba9bd5f1f9f27dc85e6102622
Matheus Reinaldo <160988681+reinaldo-matheus@users.noreply.github.com>
2025-09-17 01:55:55 +0000

    adds: atualiza configs e dependências

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,32 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+
+export default async function migrations(request, response) {
+  const defaultMigrationOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migrateMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    if (migrateMigrations.length > 0) {
+      return response.status(201).json(migrateMigrations);
+    }
+       return response.status(200).json(migrateMigrations);
+  }
+
+  return response.status(405).end();
+}
