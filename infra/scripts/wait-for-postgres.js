65790dc34396c904910ccfc9d47e1c81f1380ffe
Matheus Reinaldo <160988681+reinaldo-matheus@users.noreply.github.com>
2025-10-15 18:46:38 +0000

    adds `wait-for-postgres.js` script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,18 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout(".");
+      checkPostgres();
+      return;
+    }
+
+    console.log("\nPostgres está pronto para aceitar conexões!✅");
+  }
+}
+
+process.stdout.write("\n\naguardando postgres aceitar conexões! ⚠️");
+checkPostgres();
