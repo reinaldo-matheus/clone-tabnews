bb5a33898d57c4621e731a0481139694de897df3
Matheus Reinaldo <160988681+reinaldo-matheus@users.noreply.github.com>
2025-10-17 17:12:45 +0000

    fix: corrige uso do process.stdout no script de verificação do postgres

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -5,7 +5,7 @@ function checkPostgres() {
 
   function handleReturn(error, stdout) {
     if (stdout.search("accepting connections") === -1) {
-      process.stdout.write("."); // Correção aqui
+      process.stdout.write("."); 
       checkPostgres();
       return;
     }
